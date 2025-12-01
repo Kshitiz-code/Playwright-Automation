0c06bc582f5f16ede110acec925c677077c5c5ee
Kshitiz Gupta <kshitizg786@gmail.com>
2025-12-01 16:45:38 +0530

    Failed test

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -8,7 +8,12 @@ test("My Second Test", async function({ page }) {
     expect(100).toBe(100)
 })
 
-// Failing test
+// Failing test 1
 test("My Third Test (Intentional Failure)", async function({ page }) {
     expect(5).toBe(10)  // This will fail
+})
+
+// Failing test 2
+test("My Fourth Test (Intentional Failure)", async function({ page }) {
+    expect("hello").toBe("world")  // This will fail
 })
